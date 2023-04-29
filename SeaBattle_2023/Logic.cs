4e7dd626ed8109529a433651cf0de06418f28d09
vlhells <->
2023-04-29 18:33:16 +0300

    - Threedecks works.

--- SeaBattle_2023/Logic.cs
+++ SeaBattle_2023/Logic.cs
@@ -48,91 +48,91 @@ namespace SeaBattle_2023
 
         private static void SpawnShips(char[,] field, List<Ship> ships)
         {
-            //for (int i = 0; i < 4; i++) // Спавн однопалубных кораблей.
-            //{
-            //    ships.Add(new Ship());
+            for (int i = 0; i < 4; i++) // Спавн однопалубных кораблей.
+            {
+                ships.Add(new Ship());
 
-            //    int x;
-            //    int y;
+                int x;
+                int y;
 
-            //    do
-            //    {
-            //        x = random.Next(0, 10);
-            //        y = random.Next(0, 10);
-            //    } while (!CheckShipBoundary(field, (x,y)));
+                do
+                {
+                    x = random.Next(0, 10);
+                    y = random.Next(0, 10);
+                } while (!CheckShipBoundary(field, (x, y)));
 
-            //    field[x, y] = '*';
-            //    ships[ships.Count - 1].Decks.Add((x, y));
-            //}
+                field[x, y] = '*';
+                ships[ships.Count - 1].Decks.Add((x, y));
+            }
 
-            //for (int i = 0; i < 3; i++) // Спавн двухпалубных кораблей.
-            //{
+            for (int i = 0; i < 3; i++) // Спавн двухпалубных кораблей.
+            {
                 ships.Add(new Ship());
                 List<(int x, int y)> pre_decks_coords_list = new List<(int x, int y)>();
 
-                //    do
-                //    {
-                //        int x_null = -1;
-                //        int y_null = -1;
+                do
+                {
+                    int x_null = -1;
+                    int y_null = -1;
 
-                //        int coin = random.Next(0, 2);
-                //        switch (coin)
-                //        {
-                //            case 0:
-                //                x_null = random.Next(0, 10); // Закрепляем Ох.   
-                //                break;
+                    int coin = random.Next(0, 2);
+                    switch (coin)
+                    {
+                        case 0:
+                            x_null = random.Next(0, 10); // Закрепляем Ох.   
+                            break;
 
-                //            case 1:
-                //                y_null = random.Next(0, 10);
-                //                break;
-                //        }
+                        case 1:
+                            y_null = random.Next(0, 10);
+                            break;
+                    }
 
-                //        pre_decks_coords_list.Clear();
-                //        switch (x_null)
-                //        {
-                //            case -1: // Значит, закреплена ось Оу ==> генерим с разными х, но одним у.
-                //                x_null = random.Next(0, 10);
-                //                pre_decks_coords_list.Add((x_null, y_null));
+                    pre_decks_coords_list.Clear();
+                    switch (x_null)
+                    {
+                        case -1: // Значит, закреплена ось Оу ==> генерим с разными х, но одним у.
+                            x_null = random.Next(0, 10);
+                            pre_decks_coords_list.Add((x_null, y_null));
 
-                //                for (int z = 0; z < 1; z++) // Двухпалубник (+1 палуба)
-                //                {
-                //                    int dx = 1;
-                //                    if (x_null + dx < field.GetLength(0) && field[x_null + dx, y_null] == '.')
-                //                        pre_decks_coords_list.Add((x_null + dx, y_null));
-                //                    else if (x_null - dx >= 0 && field[x_null - dx, y_null] == '.')
-                //                    {
-                //                        pre_decks_coords_list.Add((x_null - dx, y_null));
-                //                    }
-                //                }
-                //                break;
+                            for (int z = 0; z < 1; z++) // Двухпалубник (+1 палуба)
+                            {
+                                int dx = 1;
+                                if (x_null + dx < field.GetLength(0) && field[x_null + dx, y_null] == '.')
+                                    pre_decks_coords_list.Add((x_null + dx, y_null));
+                                else if (x_null - dx >= 0 && field[x_null - dx, y_null] == '.')
+                                {
+                                    pre_decks_coords_list.Add((x_null - dx, y_null));
+                                }
+                            }
+                            break;
 
-                //            default:
-                //                y_null = random.Next(0, 10);
-                //                pre_decks_coords_list.Add((x_null, y_null));
+                        default:
+                            y_null = random.Next(0, 10);
+                            pre_decks_coords_list.Add((x_null, y_null));
 
-                //                for (int z = 0; z < 1; z++) // Двухпалубник (+1 палуба)
-                //                {
-                //                    int dy = 1;
-                //                    if (y_null + dy < field.GetLength(1) && field[x_null, y_null + dy] == '.')
-                //                        pre_decks_coords_list.Add((x_null, y_null + dy));
-                //                    else if (y_null - dy >= 0 && field[x_null, y_null - dy] == '.')
-                //                    {
-                //                        pre_decks_coords_list.Add((x_null, y_null - dy));
-                //                    }
-                //                }
-                //                break;
-                //        }
-                //        //Program.Draw(_playersField);
-                //    } while (!CheckShipBoundary(field, pre_decks_coords_list));
+                            for (int z = 0; z < 1; z++) // Двухпалубник (+1 палуба)
+                            {
+                                int dy = 1;
+                                if (y_null + dy < field.GetLength(1) && field[x_null, y_null + dy] == '.')
+                                    pre_decks_coords_list.Add((x_null, y_null + dy));
+                                else if (y_null - dy >= 0 && field[x_null, y_null - dy] == '.')
+                                {
+                                    pre_decks_coords_list.Add((x_null, y_null - dy));
+                                }
+                            }
+                            break;
+                    }
+                    //Program.Draw(_playersField);
+                } while (!CheckShipBoundary(field, pre_decks_coords_list));
 
-                //    ships[ships.Count - 1].Decks.AddRange(pre_decks_coords_list);
-                //    foreach (var deck in ships[ships.Count - 1].Decks)
-                //    {
-                //        field[deck.x, deck.y] = '*';
-                //    }
-            //}
+                ships[ships.Count - 1].Decks.AddRange(pre_decks_coords_list);
+                foreach (var deck in ships[ships.Count - 1].Decks)
+                {
+                    field[deck.x, deck.y] = '*';
+                }
+            }
 
-            TestSpawnSH(field, ships);
+            SpawnThreedecks(field, ships);
         }
 
         private static bool CheckShipBoundary(char[,] field, List<(int x, int y)> pre_coords)
@@ -183,7 +183,7 @@ namespace SeaBattle_2023
 
         }
 
-        private static void TestSpawnSH(char[,] field, List<Ship> ships)
+        private static void SpawnThreedecks(char[,] field, List<Ship> ships)
         {
             for (int i = 0; i < 2; i++) // Спавн trehpal кораблей.
             {
@@ -215,13 +215,18 @@ namespace SeaBattle_2023
                             pre_decks_coords_list.Add((x_null, y_null));
 
                             int dx = 2;
+                            string vector = "";
                             for (int h = 0; h < 2; h++)
                             {
-                                if (x_null + dx < field.GetLength(0) && field[x_null + dx, y_null] == '.')
+                                if (x_null + dx < field.GetLength(0) && field[x_null + dx, y_null] == '.' && vector != "left")
+                                {
                                     pre_decks_coords_list.Add((x_null + dx, y_null));
-                                else if (x_null - dx >= 0 && field[x_null - dx, y_null] == '.')
+                                    vector = "right";
+                                }
+                                else if (x_null - dx >= 0 && field[x_null - dx, y_null] == '.' && vector != "right")
                                 {
                                     pre_decks_coords_list.Add((x_null - dx, y_null));
+                                    vector = "left";
                                 }
                                 dx--;
                             }
